314d13c5d2eaecd788b2ccea701936b5ca56557f
ebear11 <ebear11@lewiscass.net>
2023-02-06 16:07:28 -0500

    added to container and changed toggle

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,13 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.autos.Auto;
-import frc.robot.commands.ToggleArm;
+import frc.robot.commands.ArmDown;
+import frc.robot.commands.PIDRamp;
+import frc.robot.commands.ArmDown;
+import frc.robot.commands.ArmUp;
 import frc.robot.commands.ToggleClamper;
 import frc.robot.commands.ToggleExtender;
 import frc.robot.commands.ToggleFlipper;
@@ -25,7 +30,7 @@ import frc.robot.subsystems.Swerve;
 public class RobotContainer {
     /* Controllers */
     private final Joystick driver = new Joystick(0);
-
+    private final Joystick operator = new Joystick(1);
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
     private final int strafeAxis = XboxController.Axis.kLeftX.value;
@@ -35,13 +40,21 @@ public class RobotContainer {
     private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
     private final JoystickButton PID = new JoystickButton(driver, XboxController.Button.kX.value);
+    private final POVButton liftUp = new POVButton(operator, 0);
+    private final POVButton liftDown = new POVButton(operator, 180);
+    private final JoystickButton tiltToggle = new JoystickButton(operator, 5);
+    private final JoystickButton clampToggle = new JoystickButton(operator, 2);
+    private final JoystickButton extendToggle = new JoystickButton(operator, 3);
+
+    
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
     private final ArmSubsystem armSubsystem = new ArmSubsystem();
-    private Command PIDRamp = new frc.robot.commands.PIDRamp(s_Swerve).repeatedly();
-    private Command armUp = new ToggleArm(armSubsystem).repeatedly();
+    private Command PIDRamp = new PIDRamp(s_Swerve).repeatedly();
+    private Command ArmDown = new ArmDown(armSubsystem);
+    private Command ArmUp = new ArmUp(armSubsystem);
     private Command extend = new ToggleExtender(armSubsystem);
-    private Command flipUp = new ToggleFlipper(armSubsystem);
+    private Command flip = new ToggleFlipper(armSubsystem);
     private Command clamp = new ToggleClamper(armSubsystem);
     
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -70,6 +83,11 @@ public class RobotContainer {
         /* Driver Buttons */
         zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
         PID.whileTrue(PIDRamp);
+        liftUp.onTrue(ArmUp);
+        liftDown.onTrue(ArmDown);
+        extendToggle.onTrue(extend);
+        clampToggle.onTrue(clamp);
+        tiltToggle.onTrue(flip);
     }
 
     /**
--- src/main/java/frc/robot/commands/ArmDown.java
+++ src/main/java/frc/robot/commands/ArmDown.java
@@ -1,21 +1,18 @@
 package frc.robot.commands;
 
-import frc.robot.Constants;
 import frc.robot.subsystems.ArmSubsystem;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 
-public class ToggleArm extends CommandBase {    
+public class ArmDown extends CommandBase {    
     private final ArmSubsystem armSubsystem;
-    Joystick stick = new Joystick(1);
-    public ToggleArm(ArmSubsystem armSubsystem) {
+    public ArmDown(ArmSubsystem armSubsystem) {
         this.armSubsystem = armSubsystem;
         addRequirements(armSubsystem);
     }
 
     @Override
     public void execute() {
-        armSubsystem.moveArm(Constants.armSetpoint, stick.getRawAxis(0));       
+    armSubsystem.moveArm(10000, -.5);       
     }
 }
